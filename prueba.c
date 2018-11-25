f1c23dcfa0ae55e0016feda54f1f88d7f3fa5690
riruriruriru <isaac.espinoza@usach.cl>
2018-11-24 21:14:22 -0300

    uwu 2

--- prueba.c
+++ prueba.c
@@ -15,6 +15,48 @@ typedef struct Casilla{
 	
 	}Casilla;
 
+void getArguments(int argc, char *argv[], int *numHijos, int *flag){
+	int flags, opt;
+	char *aux3;
+	aux3 = malloc(10*sizeof(char));
+	if(argc <3){//si se ingresa un numero de argumentos menor a 3, se finaliza la ejecucion del programa
+		printf("Se ingreso un numero incorrecto de argumentos\n");
+		exit(EXIT_FAILURE);
+		}
+	int nChild = -1;
+	flags = 0;
+	while ((opt = getopt(argc, argv, "mh:")) != -1) {
+	   switch (opt) {
+	   case 'm'://se busca el flag -m, en caso de ser encontrado se setea el valor flags = 1, no se considera lo que se ingrese despues del flag -m
+		   flags = 1;
+		   break;
+	   case 'h': //se busca el flag -h
+		   nChild = strtol(optarg, &aux3, 10);//se parsea el argumento ingresado junto al flag -h a entero
+		   if(optarg!=0 && nChild==0){//si no se ingresa un argumento junto a -h o si no se logra parsear el argumento ingresado, se considera como invalido
+				fprintf(stderr, "Uso correcto: %s [-h nchild] [-m]\n", argv[0]);
+				exit(EXIT_FAILURE);
+			   }
+		   //printf("optarg: %s\n", optarg);
+		   break;
+	   default: /* '?' */
+		   fprintf(stderr, "Uso correcto: %s [-h nchild] [-m]\n",
+				   argv[0]);
+		   exit(EXIT_FAILURE);
+	   }
+	}
+
+	if(flags==1){//si se encontro un flag -m, se setea la variable global flag = 1, respecto al scope del proceso principal
+		(*flag) = 1;
+		}
+	(*numHijos) = nChild; //se iguala la variable numHijos a nChild
+	if(nChild<0){
+		fprintf(stderr, "Usage: %s [-h nchild] [-m]\n", argv[0]); //si la cantidad de hijos es negativa, se retorna un error
+		exit(EXIT_FAILURE);
+		}
+
+
+}
+
 void darMemoria(Casilla ***tabla, int row, int col){
 	int i = 0;
 	*tabla = (Casilla**)malloc(row*sizeof(Casilla));
